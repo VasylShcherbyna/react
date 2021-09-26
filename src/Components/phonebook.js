0a678bf05d89e2906ad50917658fc2b039e3ab78
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-09-26 08:25:16 +0200

    start

--- src/Components/phonebook.js
+++ src/Components/phonebook.js
@@ -1,12 +1,11 @@
-import React from "react";
+import {useState} from "react";
 import PropTypes from "prop-types";
 
 
-class Phonebook extends React.Component {
-  state = {
-    name: "",
-    number: "",
-  };
+export default function Phonebook() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
 
   onChange = (e) => {
     const { name, value } = e.target;
@@ -35,7 +34,7 @@ class Phonebook extends React.Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
-            value={this.state.name}
+            value={name}
             onChange={this.onChange}
           />
         </label>
@@ -44,7 +43,7 @@ class Phonebook extends React.Component {
           <input
             type="text"
             name="number"
-            value={this.state.number}
+            value={number}
             onChange={this.onChange}
           />
         </label>
